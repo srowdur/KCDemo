e9e21586f9e83e691a279496b834cf2e6fdeb377
Srinivas Rowdur <srinivasrowdur@icloud.com>
2018-09-27 13:58:28 -0500

    Initial Commit

---
+++ KCDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KCDemo
+//
+//  Created by Srinivas Rowdur on 27/09/18.
+//  Copyright © 2018 Srinivas Rowdur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
